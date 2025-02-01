b4aad8f284f84b3d4e4da535684e9c50c50674aa
baheroz2003 <baherozzeya123456789@gmail.com>
2025-02-01 17:01:56 +0530

    my ml files

---
+++ 1.py
@@ -0,0 +1,30 @@
+# AI>ML>DEEP LEARNING
+# artificial intelligence is a branch of cs that is
+# concerned with building smart and intelligent machines
+#machine learning is atechnique to implement ai that can learn from the data by themselves without being explicitly programmed
+#deep learning is a subfield of ml that uses ann to learn from the data
+#ann that is artificial neural network is a mathematical model
+#in supervised learning,the ml algorithm learns from labelled data
+#in unsupervised learning,the ml algorithm learns from unlabelled data
+#reinforcement learning is an area of machine learning concerned with how intelligent agaents take actions in an environment to maximize it rewards that are environment,agent,action and the last one is reward 
+#types of supervised learning is classification and regression in which classification is about predicting a class or discrete value.for eg male and female ,true or false
+#suppose we have to identify it is dog or cat that can be used in classification
+#in regression for this temperature there will be how much rainfall in cm that is in decimal value
+#classification includes:decision tree classification,random forest classification,k-nearest neighbour
+#regression includes logistic,polynomial,supoort vector
+#types of unsupervised learning includes clustering(grouping similar data points) and association(find relationship between data points)
+#in clustering we have user data ,there we have different cluster for each data based on the user exeprience
+#in association we have some data from which we predict relation for other data
+#in which we have kmeans clustering,heirarichal clustering,principal component analysis,apriori,and eclat
+#deep learning uses ann
+#there we have defined layers are input layer,hidden layer 1,hidden layer 2,output layer
+#deep learning is faster than machine learning because it combines feature extraction+classification both at single step
+#deep mind is a company which primilarly uses deep learning and it creates game like chess 
+#using it you can detect diabetic retinopathy or not
+#application of deep learning are healthcare,autonomous car(tesla),computer vision that is image processing technique and natural language processing like chatbot like siri
+
+
+
+
+
+
---
+++ 2.py
@@ -0,0 +1,83 @@
+#in google colab if u write cat/proc/meminfo
+#those libraries were not installed u can install it
+# #pseudo code
+# def searchMinFromList(L, n):
+#     minValue = L[0]
+#     idx = 0
+#     for i in range(1, n):
+#         if L[i] < minValue:
+#             minValue = L[i]
+#             idx = i
+#     return minValue, idx
+
+# def sortList(L, n):
+#     L2 = []
+#     while len(L) > 0:
+#         minValue, idx = searchMinFromList(L, len(L))
+#         L2.append(minValue)
+#         del L[idx]  
+#     return L2
+#why python is best for data science
+#beginner friendly
+#versatile and flexible
+#most popular in machine learning world
+#most mature package libraries around
+#easy to code
+#different ides jupyter notebook,pycharm,colab
+#variables is a name given to memory location
+#example:x=2
+#del (x)
+#print(x)
+#learn some operator i.e ==,!=,<,>
+#%whos tell you value of the variables not,or,and also
+#round function is also there
+#divmod(27,5) will tell u about the quotient and reminder in tuple
+#isinstance(3,int)=>it will return true
+#power function as->2**4 or pow(2,4)
+#pow(x,y,z) will give x to power y and remainder z
+#i.e xtoy%z
+#input is used to take input
+#help(function name )it will tell you how to use
+#control flow includes if else and elif with this
+#symbol :
+#proper indentation should be followed
+#nested it also there
+#while loop  syntax:
+# n=input('max iterations')
+# i=1
+# while(i<n):
+#     print(i)
+#     i=i+1
+# print('done')
+#pass is used to don't do at exact lets it be incrementing,just move on i.e do nothing
+#break is used to exit the loop
+#continue to skip the iteration
+#l=[]
+# for i in range(1,n,step):
+#     print(i)
+#     L.append(i**2)
+# print(L)
+#suppose we have a list where we have to find minimum
+# l=[1,2,4]
+# m=l[0]
+# for i in l:
+#     if i<m:
+#         m=i
+# print(m)
+#the function is a block of code that can be reused in a program
+# x=int(input('enter yhe number'))
+# def fact(x):
+#     if(x==0 or x==1):
+#         return 1
+#     else:
+#         return x*fact(x-1)
+
+
+
+
+
+
+
+
+
+
